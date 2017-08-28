28ffd1c7aa15f0edfbf62c795a48e049f2d44dab
王子豪 <ZihaoWang@wangzihaos-MacBook-Air.local>
2017-08-28 21:16:23 +0800

    Initial Commit

---
+++ Data Structure/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Data Structure
+//
+//  Created by 王子豪 on 2017/8/28.
+//  Copyright © 2017年 王子豪. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
