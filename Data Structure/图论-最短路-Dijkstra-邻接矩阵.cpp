90e774166eb17681039a955c4dcfb7743eea57a9
wzh1998 <13001009107@yeah.net>
2018-04-01 23:39:27 +0800

    DS

--- Data Structure/图论-最短路-Dijkstra-邻接矩阵.cpp
+++ Data Structure/图论-最短路-Dijkstra-邻接矩阵.cpp
@@ -22,9 +22,10 @@ int d[MAX_V];
 bool used[MAX_V];
 int V;
 
-void dijkstra(){
+void dijkstra(int s){
     fill(d, d + V, INF);
     fill(used,used + V,false);
+    d[s] = 0;
     while(true){
         int v = -1;
         for(int u = 0;u < V;u++){
